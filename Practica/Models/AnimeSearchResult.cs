850a9ac886ee88dae037b8a9e3eb65ab9d4a2e49
TABORDA <USUARIO@DESKTOP-HN7KPIJ>
2025-03-19 11:21:25 -0500

    Add project files.

---
+++ Practica/Models/AnimeSearchResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Practica.Models
+{
+    public class AnimeSearchResult
+    {
+        [JsonPropertyName("data")]
+        public List<Anime> Animes { get; set; } = new();
+    }
+
+    public class Anime
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("score")]
+        public double? Score { get; set; }
+
+        [JsonPropertyName("images")]
+        public AnimeImages Images { get; set; } = new();
+    }
+
+    public class AnimeImages
+    {
+        [JsonPropertyName("jpg")]
+        public ImageUrls Jpg { get; set; } = new();
+    }
+
+    public class ImageUrls
+    {
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
---
+++ Practica/Services/AnimeService.cs
@@ -0,0 +1,20 @@
+﻿namespace Practica.Services;
+    using Practica.Models;
+
+
+    public class AnimeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AnimeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<AnimeSearchResult?> SearchAnime(string query)
+        {
+            return await _httpClient.GetFromJsonAsync<AnimeSearchResult>(
+                $"https://api.jikan.moe/v4/anime?q={query}");
+        }
+    }
+
